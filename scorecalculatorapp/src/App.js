2c2e6e3d0a65136e29a0cdf45a21b905a98c8888
Neeti Sharma <netzie9@gmail.com>
2026-06-19 18:05:25 +0530

    Cognizant Week-1 React Projects

---
+++ scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "../src/Component/CalculateScore";
+function App(){
+  return(
+    <div>
+      <CalculateScore Name={"Steve"}
+      School={"DNV Public School"}
+      total={284}
+      goal={3}
+      />
+    </div>
+  );
+}
+export default App;
+
