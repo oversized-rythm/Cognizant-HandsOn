2e23e626f916dc865c328d43ff61e1ea6e957219
Neeti Sharma <netzie9@gmail.com>
2026-07-09 17:17:56 +0530

    Exercise 7 : Financial Forecasting

---
+++ Data_Structure/Exercise-7-FinancialForecasting/FinancialForecast.java
@@ -0,0 +1,13 @@
+public class FinancialForecast {
+    // Recursive method to calculate future value
+    public static double futureValue(double currentValue, double growthRate, int years) {
+
+        // Base case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive call
+        return futureValue(currentValue, growthRate, years - 1) * (1 + growthRate);
+    }    
+}
